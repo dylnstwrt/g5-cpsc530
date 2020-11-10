4755650cc2b5e7b943ae72dd8ae6cd253ed68894
dylnstwrt <dylnstwrt@gmail.com>
2020-11-10 02:44:19 -0700

    general file loading; testing nist

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import csv
+from nist import nist_entropy
+import zxcvbn 
+
+def main():
+    with open("./resources/rockyou.txt", "r", encoding="ISO-8859-1") as file:
+        for password in file:
+            ...
+            
+
+
+
+if __name__ == "__main__":
+    main()
